7587bb24ea0d722f8b05539f0af85baf530421a0
moferaisreal <marcelovprm@gmail.com>
2026-02-03 22:11:42 -0300

    fix:organizando a estilização

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
-
-import './global.css'
+import EstiloGLobal, { Container } from './styles'
 
 function App() {
   return (
     <>
+      <EstiloGLobal />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
